1933460411990968279f36a1bacda72152c3ea98
Waabuffet <dominousas@gmail.com>
2023-12-17 22:14:40 +0100

    updated create-new scenario

--- laravel.sh
+++ laravel.sh
@@ -9,6 +9,7 @@
 # new $db_name
 
 laravel_path=/home/do/laravel
+laravel_command="docker-compose -f $laravel_path/docker-compose.yml exec myapp"
 
 if [ -z "$1" ]
 then
@@ -16,26 +17,23 @@ then
 else
 	case $1 in
 		"server")
-			if [ -z "$2" ]
-			then
+			if [ -z "$2" ];then
 				echo "Please specify 'start' or 'stop' in the second argument"
 			else
 				case $2 in
 					"start")
-						if [ -z "$3" ] && [ ! -f $(pwd)/website/.env ]
-						then
+						if [ -z "$3" ] && [ ! -f $(pwd)/website/.env ];then
 							echo "Please specify the database name in the third argument"
 						else
 							db_name=""
 							mydir=$(pwd)
-							if [ -z "$3" ]
-							then
+							if [ -z "$3" ];then
 								. $mydir/website/.env
 								db_name=$DB_DATABASE
 							else
 								db_name=$3
 							fi
-							$laravel_path/replace-env.sh $laravel_path $mydir $db_name
+							$laravel_path/replace-env.sh $laravel_path $mydir $db_name $4
 							docker-compose -f $laravel_path/docker-compose.yml --env-file $laravel_path/.env up -d
 						fi
 						;;
@@ -46,51 +44,88 @@ else
 			fi
 			;;
 		"artisan")
-			if [ ! "$(docker ps -q -f name=laravel_app)" ]
-			then
+			if [ ! "$(docker ps -q -f name=laravel_app)" ];then
 				echo "laravel container is not running"
 			else
-				docker-compose -f $laravel_path/docker-compose.yml exec myapp php artisan "${@:2}"
+				$laravel_command php artisan "${@:2}"
 			fi
 			;;
 		"tinker")
-			if [ ! "$(docker ps -q -f name=laravel_app)" ]
-			then
+			if [ ! "$(docker ps -q -f name=laravel_app)" ];then
 				echo "laravel container is not running"
 			else
-				docker-compose -f $laravel_path/docker-compose.yml exec myapp php artisan tinker
+				$laravel_command php artisan tinker
 			fi
 			;;
 		"migrate")
-			if [ ! "$(docker ps -q -f name=laravel_app)" ]
-			then
+			if [ ! "$(docker ps -q -f name=laravel_app)" ];then
 				echo "laravel container is not running"
 			else
-				docker-compose -f $laravel_path/docker-compose.yml exec myapp php artisan migrate:fresh $2
+				$laravel_command php artisan migrate:fresh $2
 			fi
 			;;
+		"auth")
+			$laravel_command composer require laravel/ui
+			$laravel_command php artisan ui bootstrap --auth
+			$laravel_command npm install
+
+			# this is included twice since the first time it will throw an error and install dependencies, it will work the second time
+			$laravel_command npm run build
+			#$laravel_command npm run dev	
+			;;
 		"composer")
-			if [ -z "$2" ]
-			then
+			if [ -z "$2" ];then
 				echo "please specify which composer package you want to install in the second argument"
 			else
-				if [ ! "$(docker ps -q -f name=laravel_app)" ]
-				then
+				if [ ! "$(docker ps -q -f name=laravel_app)" ];then
 					echo "laravel container is not running"
 				else
-					docker-compose -f $laravel_path/docker-compose.yml exec myapp composer require $2
+					$laravel_command composer require $2
 				fi
 			fi
 			;;
+		"npm")
+			if [ -z "$2" ];then
+				echo "please specify which npm command to run"
+			else
+				$laravel_command npm "${@:2}"
+			fi
+			;;
 		"new")
-			if [ -z "$2" ]
-			then
+			if [ -z "$2" ];then
 				echo "Please specify the database name in the second argument"
 			else
 				db_name=$2
 				mydir=$(pwd)
-				$laravel_path/create-new.sh $laravel_path $mydir $db_name
+
+				echo "creating project"
+				mkdir $mydir/mysql
+				mkdir $mydir/website # if we dont create this using our user, docker will create it from root and it will prevent us from proceeding due to lack of privileges
+				$laravel_path/replace-env.sh $laravel_path $mydir $db_name $4
+				docker-compose -f $laravel_path/docker-compose.yml --env-file $laravel_path/.env up -d
+
+				echo "creating laravel file system..."
+				docker logs -f laravel_app
 			fi
 			;;
+		"help")
+			echo "Available commands:"
+			echo "laravel server start <DB_NAME> [<LARAVEL_VERSION>]"
+			echo "laravel server stop"
+			echo "laravel artisan <COMMAND>"
+			echo "laravel tinker"
+			echo "laravel migrate [--seed]"
+			echo "laravel auth"
+			echo "laravel composer <PACKAGE_NAME>"
+			echo "laravel npm <COMMAND>"
+			echo "laravel new <DB_NAME>"
+			;;
+		"test")
+			mydir=$(pwd)
+			echo "you are in dir: $mydir"
+			echo "laravel path: $laravel_path"
+			;;
+		*) #default
+			echo "$1 command invalid"
 	esac
 fi
--- replace-env.sh
+++ replace-env.sh
@@ -3,6 +3,16 @@
 laravel_path=$1
 mydir=$2
 db_name=$3
+laravel_version=8.5.22
+internal_port=3000
+
+if [ -n "$4" ]
+then
+	laravel_version=$4
+	internal_port=8000
+fi
 
 sed -i "s~PROJECT_PATH=.*~PROJECT_PATH=$mydir~g" $laravel_path/.env
 sed -i "s~DB_DATABASE=.*~DB_DATABASE=$db_name~g" $laravel_path/.env
+sed -i "s~LARAVEL_VERSION=.*~LARAVEL_VERSION=$laravel_version~g" $laravel_path/.env
+sed -i "s~LARAVEL_INTERNAL_PORT=.*~LARAVEL_INTERNAL_PORT=$internal_port~g" $laravel_path/.env
